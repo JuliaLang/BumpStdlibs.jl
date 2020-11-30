b0ace7b0989882d09361b093ae4b2c02a94a4395
Dilum Aluthge <dilum@aluthge.com>
2020-11-30 01:12:14 +0000

    Update types.jl

--- src/types.jl
+++ src/types.jl
@@ -1,10 +1,12 @@
 """
+```
 Base.@kwdef struct StdlibInfo
     name::String
     branch::String
     git_url::String
     current_shas::Dict{String, String}
 end
+```
 """
 Base.@kwdef struct StdlibInfo
     name::String
