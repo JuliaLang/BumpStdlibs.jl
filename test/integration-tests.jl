3cdf2630dc04cef370853500bf1034ca80b8f4ea
Dilum Aluthge <dilum@aluthge.com>
2020-12-03 00:14:54 +0000

    Shorten the test suite

--- test/integration-tests.jl
+++ test/integration-tests.jl
@@ -3,17 +3,6 @@
     auth = BumpStdlibs.get_auth_from_environment("BUMPSTDLIBS_TOKEN_FOR_TESTS")
     pr_branch_suffix = "-$(Random.randstring(16))"
 
-    @testset "all stdlibs" begin
-        stdlibs_to_include = "all"
-        result = bump_stdlibs(
-            julia_repo;
-            auth = auth,
-            pr_branch_suffix = pr_branch_suffix,
-            stdlibs_to_include = stdlibs_to_include,
-        )
-        @test result isa Nothing
-    end
-
     @testset "only specified stdlibs" begin
         stdlibs_to_include = "Pkg"
         result = bump_stdlibs(
@@ -25,7 +14,7 @@
         @test result isa Nothing
     end
 
-    @testset "Override the default branch" begin
+    @testset "Override the default branch of the upstream JuliaLang/julia repo" begin
         julia_repo_default_branch = "master"
         stdlibs_to_include = "Pkg"
         result = bump_stdlibs(
