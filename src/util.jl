aea60331a0600b18d399834bd31abe892108069a
Dilum Aluthge <dilum@aluthge.com>
2021-11-06 22:06:26 -0400

    Fix the clickable stdlib URLs in the PR descriptions

--- src/util.jl
+++ src/util.jl
@@ -1,22 +1,17 @@
 function _try_extract_clickable_url_from_git_url(git_url::AbstractString)
-    git_url_stripped = strip(git_url)
-    regex_1 = r"^git:\/\/github.com\/([\w\-]*?)\/([\w\-\.]*?).git$"
-    m_1 = match(regex_1, git_url_stripped)
-    if m_1 !== nothing
-        owner = m_1[1]
-        repo = m_1[2]
-        return "https://github.com/$(owner)/$(repo)"
+    git_url_stripped = convert(String, strip(git_url))::String
+    if startswith(lowercase(git_url_stripped), "https://")
+        return git_url_stripped
     end
     return nothing
 end
 
 function _git_url_to_formatted_markdown(git_url::AbstractString)
-    git_url_stripped = strip(git_url)
+    git_url_stripped = convert(String, strip(git_url))::String
     clickable_url_maybe = _try_extract_clickable_url_from_git_url(git_url_stripped)
     if clickable_url_maybe === nothing
         @warn "Could not convert Git URL into a clickable link" git_url
         return "`$(git_url_stripped)`"
     end
-    clickable_link_markdown = "[$(clickable_url_maybe)]($(clickable_url_maybe))"
-    return clickable_link_markdown
+    return clickable_url_maybe
 end
