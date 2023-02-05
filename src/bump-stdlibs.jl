8b3dda9e24d2c22464c34945afe086fb37a28daa
Dilum Aluthge <dilum@aluthge.com>
2023-02-05 06:30:27 -0500

    Fix syntax

--- src/bump-stdlibs.jl
+++ src/bump-stdlibs.jl
@@ -154,7 +154,7 @@ function _bump_single_stdlib(stdlib::StdlibInfo, config::Config, state::State)
                             "Bump invoked by: $(bumpstdlibs_sender_ping)",
                             "Powered by: [BumpStdlibs.jl](https://github.com/JuliaLang/BumpStdlibs.jl)",
                             "",
-                            "Diff: $(git_url_markdown)/compare/$(stdlib_current_commit_in_upstream)...$(stdlib_latest_commit)"
+                            "Diff: $(git_url_markdown)/compare/$(stdlib_current_commit_in_upstream)...$(stdlib_latest_commit)",
                             "",
                             "```",
                             "\$ $(strip(string(changelog_cmd), '`'))",
